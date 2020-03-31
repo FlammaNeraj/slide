5cd8deba36938dd993aa1df56f62d9d573b6e9be
João Luiz <jleaopitta@gmail.com>
2020-03-31 19:33:32 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -60,9 +60,41 @@ export default class Slide {
 
   }
 
+  slidePositionCenter(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePositionCenter(element);
+      return {
+        element,
+        position,
+      };
+    });
+  }
+
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    }
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(activeSlide.position);
+    this.slideIndexNav(index);
+    this.distance.finalPosition = activeSlide.position;
+  }
+
   init() {
     this.bindEvents();
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
